1efbf6ce280fccb74f471c10474b41dd44071738
Frodnonag22 <andrew.petersen@ucas-edu.net>
2026-08-19 09:04:22 -0600

    I made the hello world assignment

---
+++ Hello_World.py
@@ -0,0 +1,9 @@
+#Andrew Petersen Hello World
+
+name = input("Hi, you seem like a nice person, whats your name: ")
+howsItGoing = input("Hello, " + name + "! How are you? ")
+print("I am glad, " + name)
+wait = input("Give me a verb: ")
+four = input("give me a number: ")
+wall = input("Give me a part of a house: ")
+print(wait + ", " + name + ", I am going to be breaking the " + four + "th " + wall)
