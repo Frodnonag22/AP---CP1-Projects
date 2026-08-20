63733015f78e85d2f2ddeabd07a78e566ff1ad18
Frodnonag22 <andrew.petersen@ucas-edu.net>
2026-08-20 09:53:47 -0600

    I coded the assignment

---
+++ character_intro.py
@@ -0,0 +1,8 @@
+#Andrew Petersen Character Inro
+
+character_name = input("What is my name? ") #John Jonah Jameson Jr
+character_age = input("How old am I? ") #52
+character_job = input("What is my job? ") #CEO of The Daily Bugle
+character_home = input("Where do I live? ") #New York City
+
+print("I am", character_name, "stop wasting my time! I'm the", character_job, "for gosh sake and I need photos of the Spider-Man! Any", character_age, "year old who lives in", character_home, "like me could get photos of him! STOP WASTING MY TIME AND GO GET THE FRIGGIN PHOTOS! You got 5 minutes until YOU'RE FIRED! I didn't get claw my way here by coddling helpless children to get photos!")
