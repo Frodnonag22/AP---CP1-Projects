e1327f5bb5e2346769f636ce128164ae0066e65a
Frodnonag22 <Andrew.Petersen@ucas-edu.net>
2026-09-03 09:53:32 -0600

    created the software

---
+++ madlib.py
@@ -0,0 +1,30 @@
+#Andrew Petersen MadLib
+
+print("")
+animal = input("Give me an animal. ") #Asking the user for this thing.
+food_1 = input("Give me a food. ") #Asking the user for this thing.
+noun_1 = input("Give me a noun. ") #Asking the user for this thing.
+verb_1 = input("Give me a verb. ") #Asking the user for this thing.
+verb_2 = input("Give me a verb. ") #Asking the user for this thing.
+verb_3 = input("Give me a verb. ") #Asking the user for this thing.
+verb_4 = input("Give me a verb. ") #Asking the user for this thing.
+noun_2 = input("Give me a noun. ") #Asking the user for this thing.
+location_1 = input("Give me a location. ") #Asking the user for this thing.
+noun_3 = input("Give me a noun. ") #Asking the user for this thing.
+noun_4 = input("Give me a noun. ") #Asking the user for this thing.
+location_2 = input("Give me a location. ") #Asking the user for this thing.
+verb_5 = input("Give me a verb. ") #Asking the user for this thing.
+food_2 = input("Give me a food. ") #Asking the user for this thing.
+game = input("Give me a game. ") #Asking the user for this thing.
+verb_6 = input("Give me a verb. ") #Asking the user for this thing.
+noun_5 = input("Give me a noun. ") #Asking the user for this thing.
+noun_6 = input("Give me a noun. ") #Asking the user for this thing.
+plural_noun_1 = input("Give me a plural noun. ") #Asking the user for this thing.
+ing_verb = input("Give me a verb ending in \"ing\". ") #Asking the user for this thing.
+verb_7 = input("Give me a verb. ") #Asking the user for this thing.
+plural_noun_2 = input("Give me a plural noun. ") #Asking the user for this thing.
+verb_8  = input("Give me a verb. ") #Asking the user for this thing.
+
+mad_lib = "If you give a(n) " + animal + " a " + food_1 + " it is going to ask for a " + noun_1 + ". When you give it the " + noun_1 + ", it will want to " + verb_1+ ". When it is finished, it will " + verb_2 + ". Then it will " + verb_3 + " and "+ verb_4 + " to the " + noun_2 + ". Since that doesn't work out, it will want to go to " + location_1 + ". On the way, it will see a " + noun_3 + " and will want " + noun_4 + ". Then you will have to take it to the " + location_2 + ". It will " + verb_5 + ". When it is done, it will ask you for some " + food_2 + ". On the way home it will start a game of " + game + ". When you finally get home, you'll have to " + verb_6 + ". Then it will want a " + noun_5 + ". You'll have to find a " + noun_6 + " and " + plural_noun_1 + ". When it sees the " + plural_noun_1 + ", it will start " + ing_verb + ". Then it will " + verb_7 + " out of " + plural_noun_2 + ". Of course when it is finished it will want to " + verb_8 + ". So it will ask for a " + noun_1 + ". And chances are if you give it a " + noun_1 + ", it is going to want a " + food_1 + "."
+
+print(mad_lib)
