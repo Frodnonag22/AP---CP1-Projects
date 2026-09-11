ad91ea89e868d9708f7674edde624bb093f6441f
Frodnonag22 <Andrew.Petersen@ucas-edu.net>
2026-09-11 09:52:31 -0600

    notes

---
+++ Notes Folder/pseudocode_notes.py
@@ -0,0 +1,33 @@
+# Andrew Petersen Pseudocode notes
+
+print("Pseudocode is for other programmers, you able to make it in multiple languages, and for what the requitements are.")
+
+print("Pseudocode is written mostly in english")
+# This is where you write the algorithm for what the code needs to do
+
+print("GET user name from user") # asks for user input
+print("SET user name to user") # makes the variable
+print("UPDATE 'user' remove extra white space from beginning to end") # updates the variable
+print("UPDATE 'user' separate user into individual letters")
+print("UPDATE 'user' put letters back together without extra spaces")
+print("UPDATE ' user' capitalize first letter of user") 
+print("OUTPUT 'Hello' + user") # what the computer puts out
+print("END") # ends the program
+
+print("WHILE _____ is _____") #loops until condition is met
+
+print("FOR number in 1-50") # does it individually
+print("\t IF number divisible by 15") # checks condition
+print("\t \t THEN OUTPUT 'FizzBuzz'") #if condition is met, print this 
+print("\t ELSE IF number dividible by 3") # checks a different condition
+print("\t \t THEN OUTPUT 'Fizz'")
+print("\t ELSE IF number dividible by 5")
+print("\t \t THEN OUTPUT 'Buzz'")
+print("\t ELSE") # checks everything else
+print("\t \t OUTPUT number")
+print("\t END IF") # ends if statement
+print("END FOR") # ends for statement
+print("END") 
+
+print("\n Bottom up: individual pieces then put it together")
+print("Top down: build most important to least important")
