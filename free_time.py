b5ad539e5f943382735717630673defb6ed507ba
Frodnonag22 <Andrew.Petersen@ucas-edu.net>
2026-09-18 09:11:22 -0600

    made more simple

--- free_time.py
+++ free_time.py
@@ -13,33 +13,25 @@ letter_eight = random.randint(1, 26)
 letter_nine = random.randint(1, 26)
 letter_ten = random.randint(1, 26)
 letter_eleven = random.randint(1, 26)
-numbers_for_password = random.randint(0, 1000)
+numbers_for_password = random.randint(0, 2000)
+
+upper_alpha = " ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+lower_alpha = " abcdefghijklmnopqrstuvwxyz"
+
+letter_1 = upper_alpha[letter_one: letter_one + 1]
+letter_2 = lower_alpha[letter_two: letter_two + 1]
+letter_3 = lower_alpha[letter_three: letter_three + 1]
+letter_4 = lower_alpha[letter_four: letter_four + 1]
+letter_5 = lower_alpha[letter_five: letter_five + 1]
+letter_6 = upper_alpha[letter_six: letter_six + 1]
+letter_7 = lower_alpha[letter_seven: letter_seven + 1]
+letter_8 = lower_alpha[letter_eight: letter_eight + 1]
+letter_9 = lower_alpha[letter_nine: letter_nine + 1]
+letter_10 = lower_alpha[letter_ten: letter_ten + 1]
+letter_11 = upper_alpha[letter_eleven: letter_eleven + 1]
 numbers = str(numbers_for_password)
 
-letter_1 = " ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-random_letter_1 = letter_1[letter_one: letter_one + 1]
-letter_2 = " abcdefghijklmnopqrstuvwxyz"
-random_letter_2 = letter_2[letter_two: letter_two + 1]
-letter_3 = " abcdefghijklmnopqrstuvwxyz"
-random_letter_3 = letter_3[letter_three: letter_three + 1]
-letter_4 = " abcdefghijklmnopqrstuvwxyz"
-random_letter_4 = letter_4[letter_four: letter_four + 1]
-letter_5 = " abcdefghijklmnopqrstuvwxyz"
-random_letter_5 = letter_5[letter_five: letter_five + 1]
-letter_6 = " ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-random_letter_6 = letter_6[letter_six: letter_six + 1]
-letter_7 = " abcdefghijklmnopqrstuvwxyz"
-random_letter_7 = letter_7[letter_seven: letter_seven + 1]
-letter_8 = " abcdefghijklmnopqrstuvwxyz"
-random_letter_8 = letter_8[letter_eight: letter_eight + 1]
-letter_9 = " abcdefghijklmnopqrstuvwxyz"
-random_letter_9 = letter_9[letter_nine: letter_nine + 1]
-letter_10 = " abcdefghijklmnopqrstuvwxyz"
-random_letter_10 = letter_10[letter_ten: letter_ten + 1]
-letter_11 = " ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-random_letter_11 = letter_11[letter_eleven: letter_eleven + 1]
-
-generated_password = f"{random_letter_1}{random_letter_2}{random_letter_3}{random_letter_4}{random_letter_5}{random_letter_6}{random_letter_7}{random_letter_8}{random_letter_9}{random_letter_10}{random_letter_11}{numbers}"
+generated_password = f"{letter_1}{letter_2}{letter_3}{letter_4}{letter_5}{letter_6}{letter_7}{letter_8}{letter_9}{letter_10}{letter_11}{numbers}"
 
 print(generated_password)
 
@@ -54,7 +46,7 @@ else:
 while play_game == True:
     print(f"sdfasdf")
     lol = random.randint(1, 100)
-    if lol < 2:
+    if lol == 1:
         play_game = False
     elif lol == 2:
         print("OOH SOO CLOSE")
