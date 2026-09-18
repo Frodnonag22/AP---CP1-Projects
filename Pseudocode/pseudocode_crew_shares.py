2613e3b2d6734752769687eb66d9e19e3efacc44
Frodnonag22 <Andrew.Petersen@ucas-edu.net>
2026-09-18 09:51:26 -0600

    fixed dumb mistake lol

---
+++ Pseudocode/pseudocode_crew_shares.py
@@ -0,0 +1,19 @@
+# Andrew Petersen Crew Shares Pseudocode
+# 
+# IMPORT random from python
+# GET random_credit_amount between 500 and 5000
+# GET crew_numbers from user
+# 
+# CALCULATE total_crew = crew_numbers + 2
+# CALCULATE yondu_13_percent = random_credit_amount * 0.13
+# CALCULATE after_yondu = random_credit_amount - yondu_13_percent
+# CALCULATE peter_11_percent = after_yondu * 0.11
+# CALCULATE after_peter = after_yondu - peter_11_percent
+# CALCULATE split_price = total_crew / after_peter
+# CALCULATE yondu_total = yondu_13_percent + split_price
+# CALCULATE peter_total = peter_11_percent + split_price
+# 
+# DISPLAY original amount of units
+# DISPLAY yondu_total rounded to 2 decimals
+# DISPLAY peter_total rounded to 2 decimals
+# DISPLAY split_price rounded to 2 decimals
