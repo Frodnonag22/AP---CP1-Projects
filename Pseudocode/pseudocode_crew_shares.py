a3a235cbb22ca2600eb2ec6055fb8651109816e9
Frodnonag22 <andrew.petersen@ucas-edu.net>
2026-09-19 18:13:08 +0000

    fixed the stuff on the pseudocode

--- Pseudocode/pseudocode_crew_shares.py
+++ Pseudocode/pseudocode_crew_shares.py
@@ -5,15 +5,15 @@
 # GET crew_numbers from user
 # 
 # CALCULATE total_crew = crew_numbers + 2
-# CALCULATE yondu_13_percent = random_credit_amount * 0.13
-# CALCULATE after_yondu = random_credit_amount - yondu_13_percent
-# CALCULATE peter_11_percent = after_yondu * 0.11
-# CALCULATE after_peter = after_yondu - peter_11_percent
-# CALCULATE split_price = total_crew / after_peter
-# CALCULATE yondu_total = yondu_13_percent + split_price
-# CALCULATE peter_total = peter_11_percent + split_price
+# CALCULATE yondu_13_percent = random_credit_amount * 0.13 rounded to 2 decimal places
+# CALCULATE after_yondu = random_credit_amount - yondu_13_percent rounded to 2 decimal places
+# CALCULATE peter_11_percent = after_yondu * 0.11 rounded to 2 decimal places
+# CALCULATE after_peter = after_yondu - peter_11_percent rounded to 2 decimal places
+# CALCULATE split_price = after_peter / total_crew rounded to 2 decimal places
+# CALCULATE yondu_total = yondu_13_percent + split_price rounded to 2 decimal places
+# CALCULATE peter_total = peter_11_percent + split_price rounded to 2 decimal places
 # 
 # DISPLAY original amount of units
-# DISPLAY yondu_total rounded to 2 decimals
-# DISPLAY peter_total rounded to 2 decimals
-# DISPLAY split_price rounded to 2 decimals
+# DISPLAY yondu_total
+# DISPLAY peter_total
+# DISPLAY split_price
