dde50f6f8dd220ce8b449e95edab8d771d4c26a8
Frodnonag22 <Andrew.Petersen@ucas-edu.net>
2026-09-21 09:53:33 -0600

    trying to color text

---
+++ free_time.py
@@ -0,0 +1,19 @@
+# Andrew Petersen Free Time 
+
+#\033[0m: Reset all modes (styles and colors)
+#\033[1m: Set bold mode
+#\033[2m: Set dim/faint mode
+#\033[3m: Set italic mode
+#\033[4m: Set underline mode
+#\033[5m: Set blinking mode
+#\033[7m: Set inverse/reverse mode
+#\033[9m: Set strikethrough mode
+
+# \033[30m: Black
+# \033[31m: Red
+# \033[32m: Green
+# \033[33m: Yellow
+# \033[34m: Blue
+# \033[35m: Magenta
+# \033[36m: Cyan
+# \033[37m: White
