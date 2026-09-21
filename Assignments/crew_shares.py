b8516416abdfbe4c6033b53a75ffb6562a3a81ec
Frodnonag22 <Andrew.Petersen@ucas-edu.net>
2026-09-21 09:31:29 -0600

    fixed stuff for rubrick

--- Assignments/crew_shares.py
+++ Assignments/crew_shares.py
@@ -12,15 +12,13 @@ while True:
 
 total_crew = crew_numbers + 2
 yondu_13_percent = yondu_13_percent = random_credit_amount * 0.13
-yondu_13_percent = round(yondu_13_percent, 2)
 after_yondu = random_credit_amount - yondu_13_percent
 peter_11_percent = after_yondu * 0.11
-peter_11_percent = round(peter_11_percent, 2)
 after_peter = after_yondu - peter_11_percent
 split_price = after_peter / total_crew
 split_price = round(split_price, 2)
-yondu_total = yondu_13_percent + split_price
-peter_total = peter_11_percent + split_price
+yondu_total = round(yondu_13_percent) + round(split_price, 2)
+peter_total = round(peter_11_percent, 2) + round(split_price, 2)
 
 print(f"Amount of units collected: {random_credit_amount}")
 print(f"Yondu's share: {yondu_total}")
