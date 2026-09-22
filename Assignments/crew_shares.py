bf6d164dcf424c1d10778370230ca2fa51837d65
Frodnonag22 <Andrew.Petersen@ucas-edu.net>
2026-09-22 09:39:07 -0600

    fixing stuff

--- Assignments/crew_shares.py
+++ Assignments/crew_shares.py
@@ -11,14 +11,15 @@ while True:
     else:
         break
 
+credit_after_3 = random_credit_amount - (crew_numbers * 3)
 total_crew = crew_numbers + 2
-yondu_13_percent = yondu_13_percent = random_credit_amount * 0.13
-after_yondu = random_credit_amount - yondu_13_percent
+yondu_13_percent = yondu_13_percent = credit_after_3 * 0.13
+after_yondu = credit_after_3 - yondu_13_percent
 peter_11_percent = after_yondu * 0.11
 after_peter = after_yondu - peter_11_percent
 split_price = after_peter / total_crew
 split_price = round(split_price, 2)
-yondu_total = round(yondu_13_percent) + round(split_price, 2)
+yondu_total = round(yondu_13_percent, 2) + round(split_price, 2)
 peter_total = round(peter_11_percent, 2) + round(split_price, 2)
 
 print(f"Amount of units collected: {random_credit_amount}")
