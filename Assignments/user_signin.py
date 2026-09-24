54ab8b7b246a99dc262895e5925523c1dd7c5f13
Frodnonag22 <andrew.petersen@ucas-edu.net>
2026-09-24 09:53:27 -0600

    slowly going

---
+++ Assignments/user_signin.py
@@ -0,0 +1,9 @@
+# Andrew Petersen User Signin
+
+signup = input("Would you like to sign up? ").lower
+
+if signup == "yes":
+    username = input("What is your username?")
+    
+else:
+    print("Then why are you here?")
